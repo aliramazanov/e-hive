6461305655dcd72c2e4a799c8514777115f286fe
Ali Ramazanov <toaliramazanov@gmail.com>
2024-10-27 22:53:02 +0400

    clean repo

--- libs/common/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './mongo/mongo.module';
-export * from './config/config.module';
-export * from './mongo/abstract.repository';
-export * from './mongo/abstract.schema';
-export * from './logger/logger.module';
