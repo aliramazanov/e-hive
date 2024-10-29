e1d57ec7960ca4e3ec886361a54174ff9df47dfd
Ali Ramazanov <toaliramazanov@gmail.com>
2024-10-29 20:33:00 +0400

    add: new microservices & libs boilerplates

---
+++ libs/common/src/postgres/abstract.repository.ts
@@ -0,0 +1,51 @@
+import { EntityManager, FindOptionsWhere, Repository } from 'typeorm';
+import { AbstractEntity } from './abstract.entity';
+import { Logger, NotFoundException } from '@nestjs/common';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
+
+export abstract class AbstractRepository<T extends AbstractEntity<T>> {
+  protected abstract readonly logger: Logger;
+  private readonly errormsg = `The item you are looking for couldn't be found`;
+
+  constructor(
+    private readonly entityRepository: Repository<T>,
+    private readonly entityManager: EntityManager,
+  ) {}
+
+  async create(entity: T): Promise<T> {
+    return this.entityManager.save(entity);
+  }
+
+  async findOne(where: FindOptionsWhere<T>): Promise<T> {
+    const entity = await this.entityRepository.findOne({ where });
+
+    if (!entity) {
+      this.logger.warn(this.errormsg, where);
+      throw new NotFoundException(this.errormsg);
+    }
+
+    return entity;
+  }
+
+  async findOneAndUpdate(
+    where: FindOptionsWhere<T>,
+    update: QueryDeepPartialEntity<T>,
+  ): Promise<T> {
+    const result = await this.entityRepository.update(where, update);
+
+    if (!result.affected) {
+      this.logger.warn(this.errormsg, where);
+      throw new NotFoundException(this.errormsg);
+    }
+
+    return this.findOne(where);
+  }
+
+  async find(where: FindOptionsWhere<T>): Promise<T[]> {
+    return this.entityRepository.findBy(where);
+  }
+
+  async findOneAndDelete(where: FindOptionsWhere<T>) {
+    await this.entityRepository.delete(where);
+  }
+}
